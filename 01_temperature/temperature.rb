dec8e43c0763b39d310e35f91bca87b0a64857d3
Derrick Cudd <instantramen125@gmail.com>
2017-01-07 07:08:34 -0500

    added tempurature conversions between Celcius and Fahrenheit

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
-#write your code here
+def ftoc(tempurature_in_f)
+	# T(°C) = (T(°F) - 32) × 5/9
+	return (tempurature_in_f - 32) * 5/9
+end
+
+def ctof(tempurature_in_c)
+	return (tempurature_in_c.to_f * 9/5) + 32
+end
