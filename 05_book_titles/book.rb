4c21d654c46f843d1ae3f565c5762403d76d0670
Derrick Cudd <instantramen125@gmail.com>
2017-01-07 17:55:51 -0500

    titleize book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
-# write your code here
+	attr_reader :title
+
+	def title= (value)
+		value.downcase!
+		words = value.split(" ")
+		words = [words[0].capitalize] + words[1..-1].map do |word|
+			little_words = %w{a and in of the an}
+			if(little_words.include? word)
+				word
+			else
+				word.capitalize
+			end
+		end
+		@title = words.join(" ")
+	end
 end
